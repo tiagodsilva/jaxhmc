91252b95367720fcc29e579d31fc5a07985c49e0
tiagodsilva <tdsh97@gmail.com>
2026-01-11 11:59:42 +0400

    update

--- examples/rings.py
+++ examples/rings.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from jaxhmc.eval import run
 from jaxhmc.potentials import RingsPotential
 
-potential = RingsPotential(sigma=0.1)
+potential = RingsPotential()
 run(potential)
 
 plt.savefig("examples/rings_samples.png")
